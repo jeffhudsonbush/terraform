94a8c20064feb5eddd9c2f1fac04f299188e7520
Jeff Bush (PagerDuty) <jeffhudsonbush@gmail.com>
2020-10-07 15:26:59 -0400

    Update pagerduty.tf
    commit to master

--- pagerduty.tf
+++ pagerduty.tf
@@ -3,7 +3,7 @@
 # enabled to support event rule creation. Use the API key to generate the Priotiy ID's here:
 # https://api-reference.pagerduty.com/#!/Priorities/get_priorities
 # To Destroy the config run Destroy twice (perhaps a bug in the provider)
-# working with Joe C
+# working with lightstream
 ################################################################################################
 variable "pd_token" {
   type = string

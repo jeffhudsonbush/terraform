4decb1ea145299bc3147df88a9c79c826e605c1d
Jeff Bush (PagerDuty) <jeffhudsonbush@gmail.com>
2021-08-17 13:02:48 -0400

    Update pagerduty.tf
    commit

--- pagerduty.tf
+++ pagerduty.tf
@@ -3,7 +3,7 @@
 # enabled to support event rule creation. Use the API key to generate the Priotiy ID's here:
 # https://api-reference.pagerduty.com/#!/Priorities/get_priorities
 # To Destroy the config run Destroy twice (perhaps a bug in the provider)
-# working with allegra
+# working with CCI Sage and Rex/Ahead
 ###########################################################
 variable "pd_token" {
   type = string
